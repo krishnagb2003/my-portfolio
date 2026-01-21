18788304491b0062aa63c6ae01d5b36eee6a2b17
Krishna Bhadrashetti <krishnagbhadrashetti@gmail.com>
2026-01-21 14:46:14 +0000

    change the color to light orange and button colours to light orange and

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-foreground text-background hover:bg-foreground/90",
+        default: "bg-white text-primary hover:bg-white/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
