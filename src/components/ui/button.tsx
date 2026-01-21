aa63b642a92f2350aed9d9737fe57adba8ef7322
Krishna Bhadrashetti <krishnagbhadrashetti@gmail.com>
2026-01-21 14:43:22 +0000

    restore the last suggestion

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,9 +20,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline shadow-none border-0",
       },
       size: {
-        default: "h-10 px-3",
-        sm: "h-9 px-2",
-        lg: "h-11 px-6",
+        default: "h-9 px-3",
+        sm: "h-8 px-2",
+        lg: "h-10 px-5",
         icon: "h-10 w-10",
       },
     },
