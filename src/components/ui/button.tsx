f06559b9244ec85e6782e14d4798e1a281ef8dec
Krishna Bhadrashetti <krishnagbhadrashetti@gmail.com>
2026-01-21 14:18:35 +0000

    Make buttons paddin small

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,9 +20,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline shadow-none border-0",
       },
       size: {
-        default: "h-10 px-8 py-2",
-        sm: "h-9 px-6",
-        lg: "h-11 px-12",
+        default: "h-10 px-4 py-2",
+        sm: "h-9 px-3",
+        lg: "h-11 px-8",
         icon: "h-10 w-10",
       },
     },
